0d6c1d3b73796e5996c86ddabc2f0dda2842b801
dilawari2008 <dilawari2008@gmail.com>
2024-03-03 17:18:31 +0530

    For Right Icon

--- app/page.js
+++ app/page.js
@@ -38,7 +38,20 @@ export default function Home() {
           <button className="text-white mt-10 text-2xl bg-gray-800 rounded-full py-4 px-6 hover:bg-gray-400 flex justify-center items-center hover:scale-110 font-body">
             <span>Start Your Journey</span>
             <div className="mx-2">
-              <RightArrow />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                />
+              </svg>
             </div>
           </button>
         </div>
